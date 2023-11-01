f1d898ef1ce5662d06466eec9c2fa77555d176cc
KwongYam_Lie <1586754446@qq.com>
2023-11-01 21:27:51 +0800

    :init my repository.

---
+++ cmake-build-debug/CMakeFiles/JsonParse.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JsonParse.dir/main.cpp.o"
+  "CMakeFiles/JsonParse.dir/main.cpp.o.d"
+  "JsonParse"
+  "JsonParse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JsonParse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
